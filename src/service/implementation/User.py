0fab2afb3fff00f8b378e38828944282d9b65e90
E5-E5 <sasha533.laz2015@yandex.ru>
2025-03-20 18:45:11 +0300

    TGB-38 Hashing of the TgID in the database has been done

--- src/service/implementation/User.py
+++ src/service/implementation/User.py
@@ -1,6 +1,7 @@
 import repository.interface.User as userRepository
 import service.interface.UserI as userService
 import repository.connector.PGConnector as Connector
+import hashlib
 
 
 class UserService(userService.UserServiceI):
@@ -8,13 +9,26 @@ class UserService(userService.UserServiceI):
         self.connector = connector
         self.user_storage = user_storage
 
+    @staticmethod
+    def hash_id(string: str) -> str:
+        return hashlib.sha256(string.encode('utf-8')).hexdigest()
+
     def add(self, tg_id: str, chat_id: str):
-        self.user_storage.add(tg_id, chat_id)
+        hash_tg_id = self.hash_id(tg_id)
+        hash_chat_id = self.hash_id(chat_id)
+
+        self.user_storage.add(hash_tg_id, hash_chat_id)
 
     def change_time_zone(self, tg_id: str, chat_id: str, time_zone: int):
-        self.user_storage.change_time_zone(tg_id, chat_id, time_zone)
+        hash_tg_id = self.hash_id(tg_id)
+        hash_chat_id = self.hash_id(chat_id)
+
+        self.user_storage.change_time_zone(hash_tg_id, hash_chat_id, time_zone)
 
     def get_event_count(self, tg_id: str, chat_id: str) -> int:
-        result = self.user_storage.get_event_count(tg_id, chat_id)
+        hash_tg_id = self.hash_id(tg_id)
+        hash_chat_id = self.hash_id(chat_id)
+
+        result = self.user_storage.get_event_count(hash_tg_id, hash_chat_id)
         return result
 
